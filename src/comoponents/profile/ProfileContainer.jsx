d07db849c07a2c9903beb0ad9fdc501117106f09
IhorKonan <k0nanomg@gmail.com>
2022-04-02 18:46:13 +0300

    withRouter

--- src/comoponents/profile/ProfileContainer.jsx
+++ src/comoponents/profile/ProfileContainer.jsx
@@ -3,10 +3,15 @@ import * as axios from 'axios';
 import { connect } from 'react-redux';
 import { setUserProfile } from '../../redux/profile-reducer';
 import Profile from './Profile';
+import { withRouter } from 'react-router-dom';
 
 class ProfileContainer extends React.Component{
   componentDidMount() {
-        axios.get(`https://social-network.samuraijs.com/api/1.0//profile/23135`)
+    let userId = this.props.match.params.userId;
+    if(!userId){
+      userId = 2;
+    }
+        axios.get(`https://social-network.samuraijs.com/api/1.0//profile/` + userId)
             .then(response => {
                 this.props.setUserProfile(response.data);
             });
@@ -24,4 +29,6 @@ let mapStateToProps = (state) => ({
     profile: state.profilePage.profile
 })
 
-export default connect(mapStateToProps, {setUserProfile})(ProfileContainer);
+let WhitUrlDataContainerComponent = withRouter(ProfileContainer);
+
+export default connect(mapStateToProps, {setUserProfile})(WhitUrlDataContainerComponent);
