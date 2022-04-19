639b9d8a3b5e59136ed318392ccfef1fa371218f
IhorKonan <k0nanomg@gmail.com>
2022-04-19 19:43:04 +0300

    captcha

--- src/comoponents/login/Login.jsx
+++ src/comoponents/login/Login.jsx
@@ -9,7 +9,7 @@ import style from '../common/FormsControls/FormsControls.module.css';
 
 const Login = (props) => {
     const onSubmit = (formData) => {
-        props.login(formData.email, formData.password, formData.rememberMe);
+        props.login(formData.email, formData.password, formData.rememberMe, formData.captcha);
     }
     if(props.isAuth){
         return <Redirect to={'/profile'} />
@@ -17,17 +17,19 @@ const Login = (props) => {
     return (
         <div>
             <h1>Login</h1>
-            <LoginReduxForm onSubmit={onSubmit}/>
+            <LoginReduxForm onSubmit={onSubmit} captchaUrl={props.captchaUrl}/>
             
         </div>
     )
 }
-const LoginForm = ({handleSubmit, error}) => {
+const LoginForm = ({handleSubmit, error, captchaUrl}) => {
     return (
             <form onSubmit={handleSubmit}>
                 {createField("Email", "email", [required], Input)}
                 {createField("Password", "password", [required], Input, {type: "password"})}
                 {createField(null, "rememberMe", [], Input, {type: "checkbox"}, "remember me")}
+                {captchaUrl && <img src={captchaUrl}/>}
+                {captchaUrl && createField('Symbols from image', "captcha", [required], Input, {})}
                 { error && <div className={style.formErrorSumary}>
                     { error }
                 </div>}
@@ -44,6 +46,7 @@ const LoginForm = ({handleSubmit, error}) => {
 }
 const LoginReduxForm = reduxForm({ form: 'login' })(LoginForm)
 const mapStateToProps = (state) => ({
+    captchaUrl: state.auth.captchaUrl,
     isAuth: state.auth.isAuth
 })
 export default connect(mapStateToProps, {login} )(Login);
--- src/redux/auth-reducer.js
+++ src/redux/auth-reducer.js
@@ -1,18 +1,20 @@
 import { stopSubmit } from "redux-form";
-import { authAPI } from "../api/api";
+import { authAPI, secutiryAPI } from "../api/api";
 
 const SET_USER_DATA = 'auth/SET_USER_DATA';
-
+const GET_CAPTCHA_URL_SUCCESS = 'auth/GET_CAPTCHA_URL_SUCCES';
 let initialState = {
     id: null,
     email: null,
     login: null,
-    isAuth: false
+    isAuth: false,
+    captchaUrl: null
 };
 
 const authReducer = (state = initialState, action) => {
     switch(action.type){
-        case SET_USER_DATA: 
+        case SET_USER_DATA:
+        case GET_CAPTCHA_URL_SUCCESS:
             return {
                 ...state,
                 ...action.payload,
@@ -23,6 +25,7 @@ const authReducer = (state = initialState, action) => {
 }
 
 export const setAuthUserData = (userId, email, login, isAuth) => ({ type: SET_USER_DATA, payload: {userId, email, login, isAuth} });
+export const getCaptchaUrlSuccess = (captchaUrl) => ({ type: GET_CAPTCHA_URL_SUCCESS, payload: {captchaUrl} });
 
 export const getAuthUserData = () => async (dispatch) => {
     let response = await authAPI.me();
@@ -31,11 +34,14 @@ export const getAuthUserData = () => async (dispatch) => {
         dispatch(setAuthUserData(id, email, login, true));
     }
 }
-export const login = (email, password, rememberMe) => async (dispatch) => {
-    let response = await authAPI.login(email, password, rememberMe);
+export const login = (email, password, rememberMe, captcha) => async (dispatch) => {
+    let response = await authAPI.login(email, password, rememberMe, captcha);
         if (response.data.resultCode === 0) {
             dispatch(getAuthUserData());
         } else {
+            if(response.data.resultCode === 10){
+                dispatch(getCaptchaUrl());
+            }
             let message = response.data.messages.length > 0 ? response.data.messages[0] : 'Some error';
             dispatch(stopSubmit('login', {_error: message}));
         }
@@ -45,6 +51,11 @@ export const logout = () => async (dispatch) => {
         if (response.data.resultCode === 0) {
             dispatch(setAuthUserData(null, null, null, false));
         }
+}
+export const getCaptchaUrl = () => async (dispatch) => {
+    const response = await secutiryAPI.getCaptchaUrl();
+    const captchaUrl = response.data.url;
+    dispatch(getCaptchaUrlSuccess(captchaUrl));
 }
 
 
